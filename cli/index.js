7a47b7658ce920052469aba8a2796f9ad6db3caa
Torick J Davis <torickjdavis@gmail.com>
2021-04-15 18:36:29 -0600

    Convert to CommonJS Module

--- cli/index.js
+++ cli/index.js
@@ -1,59 +1,58 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-// import inquirer from 'inquirer';
-// import axios from 'axios';
+require('./config/env');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
 
-const listOptions = {
-  page: {
-    alias: 'p',
-    type: 'number',
-  },
-  limit: {
-    alias: 'l',
-    type: 'number',
-  },
-  all: {
-    alias: 'a',
-    conflicts: ['limit', 'page'],
-    type: 'boolean',
-  },
-};
-
-const argv = yargs(hideBin(process.argv))
-  .usage('$0 <cmd> [args]')
-  .command(['create', 'init', 'make', 'mk'], 'Create new resource', (yargs) =>
-    yargs
-      .command('kata', '', (argv) => console.log('Create Kata', argv))
-      .command('workshop', '', {
-        title: {
-          alias: 't',
-          demandOption: true,
-          type: 'string',
-        },
-        katas: {
-          alias: 'k',
-          demandOption: true,
-          type: 'array',
-        },
-      })
-      .demandCommand()
-  )
-  .command(['read', 'get', '$0'], 'Access individual resources', {})
-  .command(['update', 'publish', 'up'], 'Update resource', {})
-  .command(['delete', 'remove', 'rm'], 'Delete resource', {})
-  .command(['list', 'ls'], 'List resource', (yargs) =>
-    yargs
-      .command('kata', '', listOptions)
-      .command('workshop', '', listOptions)
-      .demandCommand()
-  )
+yargs(hideBin(process.argv))
+  .commandDir('cmds')
+  .demandCommand()
+  .help()
   .strict()
-  .wrap(null).argv;
-console.log(argv);
+  .wrap(yargs.terminalWidth()).argv;
 
-// import os from 'os';
-// console.log(os.homedir());
+// const listOptions = {
+//   page: {
+//     alias: 'p',
+//     type: 'number',
+//   },
+//   limit: {
+//     alias: 'l',
+//     type: 'number',
+//   },
+//   all: {
+//     alias: 'a',
+//     conflicts: ['limit', 'page'],
+//     type: 'boolean',
+//   },
+// };
+
+// const argv = yargs(hideBin(process.argv))
+//   .usage('$0 <cmd> [args]')
+//   .command(['create', 'init', 'make', 'mk'], 'Create new resource', (yargs) =>
+//     yargs
+//       .command('kata', '', (argv) => console.log('Create Kata', argv))
+//       .command('workshop', '', {
+//         title: {
+//           alias: 't',
+//           demandOption: true,
+//           type: 'string',
+//         },
+//         katas: {
+//           alias: 'k',
+//           demandOption: true,
+//           type: 'array',
+//         },
+//       })
+//       .demandCommand()
+//   )
+//   .command(['read', 'get', '$0'], 'Access individual resources', {})
+//   .command(['update', 'publish', 'up'], 'Update resource', {})
+//   .command(['delete', 'remove', 'rm'], 'Delete resource', {})
+//   .command(['list', 'ls'], 'List resource', (yargs) =>
+//     yargs
+//       .command('kata', '', listOptions)
+//       .command('workshop', '', listOptions)
+//       .demandCommand()
 
 /**
  *  <cli name> <action> [resource]
